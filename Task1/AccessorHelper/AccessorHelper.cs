e336574417a49e526bc6339b0a410ac62e1da9d8
Evgeniy Bakaev <evgeniybakaev@bk.ru>
2023-11-12 20:50:15 +0300

    add task1

---
+++ Task1/AccessorHelper/AccessorHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace NAccessorHelper
+{
+    public class AccessorHelper
+    {
+        public static Func<T, U> CreateAccessor<T, U>(string path)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var memberExpression = CreateMemberExpression(parameter, path);
+            var lambdaExpression = Expression.Lambda<Func<T, U>>(memberExpression, parameter);
+
+            return lambdaExpression.Compile();
+        }
+
+        private static Expression CreateMemberExpression(Expression expression, string path)
+        {
+            foreach (var propertyName in path.Split('.'))
+            {
+                expression = Expression.PropertyOrField(expression, propertyName);
+            }
+
+            return expression;
+        }
+    }
+}
